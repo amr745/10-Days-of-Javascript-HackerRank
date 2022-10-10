5d9cd576912c11bd5e8edb1a96ae2ffb136ae532
Ashley Richard <ashleymrichard@gmail.com>
2022-10-10 08:31:46 -0700

    adds Functions

--- day1.js
+++ day1.js
@@ -54,5 +54,38 @@ function getPerimeter(length, width) {
     return perimeter;
 }
 
-// Functions
+//Functions
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function factorial(n){
+    //base case
+    if(n == 0 || n == 1){
+        return 1;
+    //recursive case
+    }else{
+        return n * factorial(n-1);
+    }
+}
 
