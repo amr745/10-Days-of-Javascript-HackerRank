916c726d39aa6ece943959de4440e801f6ee2566
Ashley Richard <ashleymrichard@gmail.com>
2022-10-10 21:15:13 -0700

    adds Let and Const

--- day1.js
+++ day1.js
@@ -56,6 +56,41 @@ function getPerimeter(length, width) {
 
 //Functions
 
+// 'use strict';
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
+
+// let inputString = '';
+// let currentLine = 0;
+
+// process.stdin.on('data', inputStdin => {
+//     inputString += inputStdin;
+// });
+
+// process.stdin.on('end', _ => {
+//     inputString = inputString.trim().split('\n').map(string => {
+//         return string.trim();
+//     });
+    
+//     main();    
+// });
+
+// function readLine() {
+//     return inputString[currentLine++];
+// }
+// function factorial(n){
+//     //base case
+//     if(n == 0 || n == 1){
+//         return 1;
+//     //recursive case
+//     }else{
+//         return n * factorial(n-1);
+//     }
+// }
+
+// Let and Const
+
 'use strict';
 
 process.stdin.resume();
@@ -79,13 +114,25 @@ process.stdin.on('end', _ => {
 function readLine() {
     return inputString[currentLine++];
 }
-function factorial(n){
-    //base case
-    if(n == 0 || n == 1){
-        return 1;
-    //recursive case
-    }else{
-        return n * factorial(n-1);
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    
+    const PI = Math.PI;
+    
+    const r = readLine();
+    
+    // Print the area of the circle:
+    console.log(PI * Math. pow(r, 2))
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r)
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
     }
 }
-
