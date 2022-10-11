b787f40e15ae81b45b11f8611e55bb115ad62cc0
Ashley Richard <ashleymrichard@gmail.com>
2022-10-11 08:29:17 -0700

    adds Conditional Statements: Switch

--- day2.js
+++ day2.js
@@ -1,22 +1,58 @@
 //Conditional Statements: If-Else
 
-function getGrade(score) {
-    let grade;
-    if (25 < score && score <= 30) {
-         grade = "A"
-    } else if (20 < score && score <= 25) {
-         grade = "B"
-    } else if (15 < score && score <= 20) {
-         grade = "C"
-    } else if (10 < score && score <= 15) {
-         grade = "D"
-    } else if (5 < score && score <= 10) {
-         grade = "E"
-    } else if (0 <= score && score <= 5) {
-         grade = "F"
-    }
+// function getGrade(score) {
+//     let grade;
+//     if (25 < score && score <= 30) {
+//          grade = "A"
+//     } else if (20 < score && score <= 25) {
+//          grade = "B"
+//     } else if (15 < score && score <= 20) {
+//          grade = "C"
+//     } else if (10 < score && score <= 15) {
+//          grade = "D"
+//     } else if (5 < score && score <= 10) {
+//          grade = "E"
+//     } else if (0 <= score && score <= 5) {
+//          grade = "F"
+//     }
     
-    return grade;
+//     return grade;
+// }
+
+// console.log(getGrade(20))
+
+//Conditional Statements: Switch
+
+function getLetter(s) {
+    let letter;
+    switch (true) {
+        case "aeiou".includes(Array.from(s)[0]):
+            letter = "A";
+          break;
+         case "bcdfg".includes(Array.from(s)[0]):
+             letter = "B";
+           break;
+        case "hjklm".includes(Array.from(s)[0]):
+            letter = "C";
+          break;
+        case "npqrstvwxyz".includes(Array.from(s)[0]):
+            letter = "D";
+      }
+    
+      return letter;
 }
 
-console.log(getGrade(20))
+console.log(getLetter("nope"))
+
+// function getLetter(s) {
+//         // let letter;
+//         let first = Array.from(s)[0]
+//         let vowels = "aeiou"
+//             if (vowels.includes(first)){
+//                 return "A";
+//             } else {
+//                 return "NO"
+//             }
+// }
+
+// console.log(getLetter("boy"))
