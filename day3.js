8073fe6f4cf213512a59a2ef78fb9e21e1fd522e
Ashley Richard <ashleymrichard@gmail.com>
2022-10-18 23:35:38 -0700

    adds Day 3 Arrays

---
+++ day3.js
@@ -0,0 +1,47 @@
+// Arrays
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    
+    function arrayRemove(array, value) { 
+        return array.filter(function(ele){ 
+            let newArr = (ele != value)
+            return (newArr)
+        })
+    }
+            
+return Math.max(...(arrayRemove(nums, Math.max(...nums))))
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(23665));
+}
